96e98500d93a3b045e9e8fd8f5efb15c2b2eb514
Heber Quequejana <heberqc@gmail.com>
2017-12-22 17:00:20 -0500

    Emitir eventos

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
